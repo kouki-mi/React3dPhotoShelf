f52006433ce1260da333a8c6f7946367da32e2ee
koki-miu <>
2024-08-01 22:52:35 +0900

    カードコンポーネントをドラッグで回転できるようにした

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,54 +2,73 @@
 
 import { NextPage } from 'next';
 import React, { useRef, useState } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
-import { Mesh } from 'three';
+import { Canvas, useFrame, ThreeEvent, events, } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { Mesh, Vector3} from 'three';
 import Card from '@/component/card';
 
-type BoxProps = {
-  position: [x: number, y: number, z: number];
+const Rig = ({ v = new Vector3() }) => {
+  return useFrame((state) => {
+    state.camera.position.lerp(v.set(state.mouse.x / 2, state.mouse.y / 2, 10), 0.05)
+  })
 };
 
-const Box: React.FC<BoxProps> = (props) => {
-  const mesh = useRef<Mesh>(null!);
-  const [hovered, setHover] = useState(false);
+const Home: NextPage = () => {
   const [active, setActive] = useState(false);
-  useFrame(() => (mesh.current.rotation.x += 0.01));
+  //ここでisDraggingとpreviousMousePositionのstateを定義
+  const [isDragging, setIsDragging] = useState(false);
+  const previousMousePosition = useRef({ x: 0, y: 0 });
+  const mouseDelta = useRef({ x: 0, y: 0 });
 
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? 1.5 : 1}
-      onClick={() => setActive(!active)}
-      onPointerOver={() => setHover(true)}
-      onPointerOut={() => setHover(false)}
+  const handlePointerDown = (e: React.PointerEvent<HTMLDivElement>) => {
+    setIsDragging(true);
+    previousMousePosition.current = { x: e.clientX, y: e.clientY };
+  
+  }
+  const handlePointerMove = (e: React.PointerEvent<HTMLDivElement>) => {
+    if (!isDragging) return;
+    const { x, y } = previousMousePosition.current;
+    const dx = e.clientX - x;
+    const dy = e.clientY - y;
+    previousMousePosition.current = { x: e.clientX, y: e.clientY };
+    mouseDelta.current = { x: dx, y: dy };
+    console.log(dx, dy);
+  }
+
+  const handlePointerUp = () => {
+    setIsDragging(false);
+  }
+
+  return(
+    <div 
+      style={{ width: '100vw', height: '100vh' }}
+      onPointerDown={handlePointerDown}
+      onPointerMove={handlePointerMove}
+      onPointerUp={handlePointerUp}
     >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
+      <Canvas>
+        <color attach="background" args={['#ffffff']} />
+        <ambientLight />
+        {/* 縦と横に回転 */}
+        <Card position={[0, 0, 3.5]} 
+          scale={1}
+          imageFront="/ray.jpeg"
+          imageBack="/rio.jpeg"
+          animate={
+            (mesh) => {
+              console.log(isDragging)
+              if(isDragging && mouseDelta.current.x !== 0 && mouseDelta.current.y !== 0){
+                console.log("dragging");
+                mesh.rotation.x += mouseDelta.current.y * 0.01;
+                mesh.rotation.y += mouseDelta.current.x * 0.01;
+                mouseDelta.current = { x: 0, y: 0 };
+              }
+            }
+          }
+        />
+      </Canvas>
+    </div>
   );
 };
-
-const Home: NextPage = () => (
-
-  <div style={{ width: '100vw', height: '100vh' }}>
-    <Canvas>
-      <color attach="background" args={['#ffffff']} />
-      <ambientLight />
-      {/* 縦と横に回転 */}
-      <Card position={[0, 0, 3.5]} 
-        scale={1} 
-        imageFront="/ray.jpeg"
-        imageBack="/rio.jpeg"
-        animate={
-          (mesh) => {
-            mesh.rotation.y += 0.01;
-          }
-        }
-      />
-    </Canvas>
-  </div>
-);
 
 export default Home;
--- src/component/card.tsx
+++ src/component/card.tsx
@@ -1,6 +1,6 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import {TextureLoader, Mesh } from 'three';
-import { useFrame } from '@react-three/fiber';
+import { ThreeEvent, useFrame } from '@react-three/fiber';
 import { useTexture } from '@react-three/drei';
 import * as THREE from 'three';
 
@@ -20,15 +20,18 @@ type Size = [
 
 export default function Card(props: CardProps){
     const mesh = useRef<Mesh>(null!);
+    
+    useFrame(() => {
+        if (props.animate) {
+            props.animate(mesh.current);
+        }
+    })
+
     const { position, scale, imageFront, imageBack } = props;
     const textureLoaderFront = new THREE.TextureLoader().load(imageFront)
     const textureLoaderBack = new THREE.TextureLoader().load(imageBack)
     const size: Size = [scale, scale, 0.01];
-    useFrame(() => {
-        if(props.animate){
-            props.animate(mesh.current);
-        }
-    });
+    
     return (
         <mesh
             position={position}
